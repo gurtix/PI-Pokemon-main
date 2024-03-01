4b9c35299bfb6017afcb0dd42fb3e9d893761050
Gurtix <gurtix@hotmail.com>
2024-02-29 23:09:17 -0500

    modifique controllers para pasar stats, puse filtros y ordenamientos para visualizar pokemons, ya esta hecha la logica y las condiciones para crear pokemons, falta darle aspecto los checks de los tipos de pokemon

--- client/src/CreatePokemon.js
+++ client/src/CreatePokemon.js
@@ -22,6 +22,15 @@ function CreatePokemon() {
     const [sprites, setSprites] = useState({ front_default: '' });
     // const [types, setTypes] = useState([{ name: '', url: '' }]);
     const [types, setTypes] = useState([]);
+    const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [nameError, setNameError] = useState('');
+    const [heightError, setHeightError] = useState('');
+    const [weightError, setWeightError] = useState('');
+    const [ability1Error, setAbility1Error] = useState('');
+    const [ability2Error, setAbility2Error] = useState('');
+    const [spritesError, setSpritesError] = useState('');
+
 
     useEffect(() => {
         const fetchTypes = async () => {
@@ -37,10 +46,36 @@ function CreatePokemon() {
     }, []);
 
 
-
-
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        if (!name) {
+            setNameError('Falta el nombre');
+            return;
+        }
+        if (!height) {
+            setHeightError('Falta la altura');
+            return;
+        }
+        if (!weight) {
+            setWeightError('Falta el peso');
+            return;
+        }
+    
+        if (!abilities[0].ability.name) {
+            setAbility1Error('Falta la habilidad 1');
+            return;
+        }
+    
+        if (!abilities[1].ability.name) {
+            setAbility2Error('Falta la habilidad 2');
+            return;
+        }
+    
+        if (!sprites.front_default) {
+            setSpritesError('Falta el sprite');
+            return;
+        }
 
         const pokemonData = {
             name,
@@ -55,15 +90,24 @@ function CreatePokemon() {
         try {
             const response = await axios.post('http://localhost:3001/pokemons', pokemonData);
 
+
+
             if (response.status === 200) {
                 console.log('Pokémon guardado con éxito:', response.data);
+                setSuccessMessage('POKEMON CREADO!');
+                setErrorMessage('');
             } else {
                 console.error('Hubo un problema al guardar el Pokémon');
+                setErrorMessage('El nombre que intentas poner ya existe');
+                setSuccessMessage('');
             }
         } catch (error) {
             console.error('Error:', error);
+            setErrorMessage('El nombre que intentas usar ya existe!');
+            setSuccessMessage('');
         }
     };
+
 
 
 
@@ -76,27 +120,34 @@ function CreatePokemon() {
 
             <div className='list'>
                 <form onSubmit={handleSubmit}>
-                    <div>
+                    <div className='nombre'><h5>ingresa un nombre por favor</h5>
                         <input type="text" placeholder="Nombre" value={name} onChange={(e) => setName(e.target.value)} />
+                        {nameError && <p className="error-message">{nameError}</p>}
                     </div>
-                    <div>
+                    <div className='altura'>
                         <input type="text" placeholder="Altura" value={height} onChange={(e) => setHeight(e.target.value)} />
+                        {heightError && <p className="error-message">{heightError}</p>}
                     </div>
-                    <div>
+                    <div className='peso'>
                         <input type="text" placeholder="Peso" value={weight} onChange={(e) => setWeight(e.target.value)} />
+                        {weightError && <p className="error-message">{weightError}</p>}
                     </div>
-                    <div>
-                    <input type="text" placeholder="Habilidad 1" value={abilities[0].ability.name} onChange={(e) => {
-    const newAbilities = [...abilities];
-    newAbilities[0].ability.name = e.target.value;
-    setAbilities(newAbilities);
-}} />
-<input type="text" placeholder="Habilidad 2" value={abilities[1].ability.name} onChange={(e) => {
-    const newAbilities = [...abilities];
-    newAbilities[1].ability.name = e.target.value;
-    setAbilities(newAbilities);
-}} />
+                    <h5>Habilidades del pokemon</h5>
+                    <div className='habilidades'>
+                        <input type="text" placeholder="Habilidad 1" value={abilities[0].ability.name} onChange={(e) => {
+                            const newAbilities = [...abilities];
+                            newAbilities[0].ability.name = e.target.value;
+                            setAbilities(newAbilities);
+                        }} />
+                        {ability1Error && <p className="error-message">{ability1Error}</p>}
+                        <input type="text" placeholder="Habilidad 2" value={abilities[1].ability.name} onChange={(e) => {
+                            const newAbilities = [...abilities];
+                            newAbilities[1].ability.name = e.target.value;
+                            setAbilities(newAbilities);
+                        }} />
+                        {ability2Error && <p className="error-message">{ability2Error}</p>}
                     </div>
+                    <h5>Estadisticas de combate</h5>
                     <div className='stats'>
                         {stats.map((stat, index) => (
                             <div key={index}>
@@ -107,25 +158,31 @@ function CreatePokemon() {
                                 }} />
                             </div>
                         ))}
-                    </div>                    
-                    {availableTypes.map((type) => (
-    <div key={type.id}>
-        <input type="checkbox" id={type.name} value={type.name} onChange={(e) => {
-            if (e.target.checked) {
-                setTypes([...types, { name: e.target.value, url: type.url }]);
-            } else {
-                setTypes(types.filter((t) => t.name !== e.target.value));
-            }
-        }} />
-        <label htmlFor={type.name}>{type.name}</label>
-    </div>
-))}
-                    <div>
-                        <input type="text" placeholder="Sprite Front Default" value={sprites.front_default} onChange={(e) => setSprites({ front_default: e.target.value })} />
                     </div>
-                    <button type="submit">Guardar Pokémon</button>
+                    <h5>Seleccione el o los tipos del pokemon</h5>
+                    <div className='checkbox'>
 
+                        {availableTypes.map((type) => (
+                            <div key={type.id}>
+                                <input type="checkbox" id={type.name} value={type.name} onChange={(e) => {
+                                    if (e.target.checked) {
+                                        setTypes([...types, { name: e.target.value, url: type.url }]);
+                                    } else {
+                                        setTypes(types.filter((t) => t.name !== e.target.value));
+                                    }
+                                }} />
+                                <label htmlFor={type.name}>{type.name}</label>
+                            </div>
+                        ))}
+                    </div>
+                    <div className='apariencia'><h5>agregue un link de la imagen del pokemon</h5>
+                        <input type="text" placeholder="Sprite Front Default" value={sprites.front_default} onChange={(e) => setSprites({ front_default: e.target.value })} />
+                        {spritesError && <p className="error-message">{spritesError}</p>}
+                    </div>
+                    <button className='guardar' type="submit">Guardar Pokémon</button>
                 </form>
+                {successMessage && <p className='mensajeCreado'>{successMessage}</p>}
+                {errorMessage && <p className='mensajeError'>{errorMessage}</p>}
             </div>
         </div>
     );
--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -2,109 +2,242 @@ import React, { useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import "./HomePage.css";
 import backgroundImage from './homepage.jpg';
+import axios from 'axios';
 
 
 const API_URL = "http://localhost:3001/pokemons?offset=0&limit=151"
 
 function HomePage() {
-    const [allPokemons, setAllPokemons] = useState([]);
-    const [displayedPokemons, setDisplayedPokemons] = useState([]);
-    const [search, setSearch] = useState('');
-    const [offset, setOffset] = useState(0);
-    const history = useHistory();
-    const [isScrolled, setIsScrolled] = useState(false);
+  const [allPokemons, setAllPokemons] = useState([]);
+  const [displayedPokemons, setDisplayedPokemons] = useState([]);
+  const [search, setSearch] = useState('');
+  const [offset] = useState(0);
+  const history = useHistory();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [availableTypes, setAvailableTypes] = useState([]);
+  // const [pokemonTypes, setPokemonTypes] = useState([]);
+  const [selectedType, setSelectedType] = useState('');
+  const [sortCriteria, setSortCriteria] = useState('');
+  const [sortedAndFilteredPokemons, setSortedAndFilteredPokemons] = useState({
+    pokemons: [],
+    offset: 0
+  });
+  useEffect(() => {
+    console.log(offset, displayedPokemons.length);
+  }, [offset, displayedPokemons]);
 
-    useEffect(() => {
-        fetch(`${API_URL}`).then((response) => response.json()).then((data) => {
-          Promise.all(data.map(pokemon => fetch(pokemon.url).then(res => res.json())))
-            .then(pokemonDetails => {
-              setAllPokemons(pokemonDetails);
-              setDisplayedPokemons(pokemonDetails);
-            });
+
+  useEffect(() => {
+    let sortedPokemons = [...allPokemons];
+
+    switch (sortCriteria) {
+      case 'name-asc':
+        sortedPokemons.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'name-desc':
+        sortedPokemons.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case 'weight':
+        sortedPokemons.sort((a, b) => a.weight - b.weight);
+        break;
+      case 'height':
+        sortedPokemons.sort((a, b) => a.height - b.height);
+        break;
+      default:
+        break;
+    }
+
+    setDisplayedPokemons(sortedPokemons.slice(offset, offset + 20));
+  }, [sortCriteria, offset, allPokemons]);
+
+  useEffect(() => {
+    let sortedAndFiltered = [...allPokemons];
+
+    if (selectedType !== '') {
+      sortedAndFiltered = sortedAndFiltered.filter(pokemon =>
+        pokemon.types.some(typeObj => typeObj.type.name === selectedType)
+      );
+    }
+
+    switch (sortCriteria) {
+      case 'name-asc':
+        sortedAndFiltered.sort((a, b) => a.name.localeCompare(b.name));
+        break;
+      case 'name-desc':
+        sortedAndFiltered.sort((a, b) => b.name.localeCompare(a.name));
+        break;
+      case 'weight':
+        sortedAndFiltered.sort((a, b) => a.weight - b.weight);
+        break;
+      case 'height':
+        sortedAndFiltered.sort((a, b) => a.height - b.height);
+        break;
+      default:
+        break;
+    }
+
+    setSortedAndFilteredPokemons({
+      pokemons: sortedAndFiltered,
+      offset: sortedAndFilteredPokemons.offset
+    });
+  }, [sortCriteria, offset, allPokemons, sortedAndFilteredPokemons.offset, selectedType]);
+  useEffect(() => {
+    setDisplayedPokemons(sortedAndFilteredPokemons.pokemons.slice(sortedAndFilteredPokemons.offset, sortedAndFilteredPokemons.offset + 20));
+  }, [sortedAndFilteredPokemons]);
+
+
+  useEffect(() => {
+    if (selectedType === '') {
+      setDisplayedPokemons(allPokemons.slice(offset, offset + 20));
+    } else {
+      const filteredPokemons = allPokemons.filter(pokemon =>
+        pokemon.types.some(typeObj => typeObj.type.name === selectedType)
+      );
+      setDisplayedPokemons(filteredPokemons.slice(offset, offset + 20));
+    }
+  }, [selectedType, offset, allPokemons]);
+
+
+
+  useEffect(() => {
+    fetch(`${API_URL}`).then((response) => response.json()).then((data) => {
+      Promise.all(data.map(pokemon => fetch(pokemon.url).then(res => res.json())))
+        .then(pokemonDetails => {
+          setAllPokemons(pokemonDetails);
+          setDisplayedPokemons(pokemonDetails);
         });
-      },[]);
-    
-    useEffect(() => {
-        const filteredPokemons = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
-        setDisplayedPokemons(filteredPokemons.slice(offset, offset + 20));
-    }, [search, offset, allPokemons]);
+    });
+  }, []);
 
-    useEffect(() => {
-        const checkScroll = () => {
-          if (window.scrollY > 30) {
-            setIsScrolled(true);
-          } else {
-            setIsScrolled(false);
-          }
-        };
-    
-        window.addEventListener('scroll', checkScroll);
-    
-        return () => {
-          window.removeEventListener('scroll', checkScroll);
-        };
-      }, []);
+  useEffect(() => {
+    const filteredPokemons = allPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
+    setDisplayedPokemons(filteredPokemons.slice(offset, offset + 20));
+  }, [search, offset, allPokemons]);
 
-        
-
-      const loadMorePokemons = () => {
-        const newOffset = offset + 20;
-        setOffset(newOffset);
-        setDisplayedPokemons(allPokemons.slice(newOffset, newOffset + 20));
-    };
-    
-    const loadPreviousPokemons = () => {
-        const newOffset = offset - 20;
-        setOffset(newOffset);
-        setDisplayedPokemons(allPokemons.slice(newOffset, newOffset + 20));
+  useEffect(() => {
+    const checkScroll = () => {
+      if (window.scrollY > 30) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
     };
 
-    const goToStart = () => {
-        setOffset(0);
-        setDisplayedPokemons(allPokemons.slice(0, 20));
+    window.addEventListener('scroll', checkScroll);
+
+    return () => {
+      window.removeEventListener('scroll', checkScroll);
     };
-    
-    const goToEnd = () => {
-        const newOffset = Math.floor(allPokemons.length / 20) * 20;
-        setOffset(newOffset);
-        setDisplayedPokemons(allPokemons.slice(newOffset, newOffset + 20));
+  }, []);
+
+  useEffect(() => {
+    const fetchTypes = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/pokemons/types');
+        setAvailableTypes(response.data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
     };
 
-    const filteredPokemons = displayedPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
-    
-    
+    fetchTypes();
+  }, []);
+
+  const loadMorePokemons = () => {
+    setSortedAndFilteredPokemons(prevState => {
+      if (prevState.offset + 20 < prevState.pokemons.length) {
+        return { ...prevState, offset: prevState.offset + 20 };
+      }
+      return prevState;
+    });
+  };
+
+  const loadPreviousPokemons = () => {
+    setSortedAndFilteredPokemons(prevState => {
+      if (prevState.offset - 20 >= 0) {
+        return { ...prevState, offset: prevState.offset - 20 };
+      }
+      return prevState;
+    });
+  };
+
+
+  const goToStart = () => {
+    console.log("goToStart is called");
+    setSortedAndFilteredPokemons(prevState => ({
+      ...prevState,
+      offset: 0
+    }));
+  };
+
+  const goToEnd = () => {
+    console.log("goToEnd is called");
+    setSortedAndFilteredPokemons(prevState => ({
+      ...prevState,
+      offset: Math.floor(prevState.pokemons.length / 20) * 20
+    }));
+  };
+
+
+
+  const filteredPokemons = displayedPokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
+
+
   return (
-    <div className='background'style={{ backgroundImage: `url(${backgroundImage})` }}>
+    <div className='background' style={{ backgroundImage: `url(${backgroundImage})` }}>
       <header className={`header ${isScrolled ? 'hidden' : ''}`}>
         <nav>
-        <button onClick={() => history.push('/post')}>Crear Pokemon</button>
+          <button onClick={() => history.push('/post')}>Crear Pokemon</button>
         </nav>
         <div className='search'>
-        <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Buscar..." />
+          <input type="text" value={search} onChange={e => setSearch(e.target.value)} placeholder="Buscar..." />
         </div>
       </header>
+        <div>
+      <h4>Filtrar</h4>
+
+        </div>
+      <div className='tipos'>
+        <select className="ordenamiento" value={sortCriteria} onChange={e => setSortCriteria(e.target.value)}>
+          <option value="">Pokedex</option>
+          <option value="name-asc">Por nombre (A-Z)</option>
+          <option value="name-desc">Nombre (Z-A)</option>
+          <option value="weight">Peso</option>
+          <option value="height">Altura</option>
+        </select>
+
+        <select className='tiposfiltrados' onChange={e => setSelectedType(e.target.value)}>
+          <option value="">Todos los tipos</option>
+          {availableTypes.map(type => (
+            <option key={type.name} value={type.name}>
+              {type.name}
+            </option>
+          ))}
+        </select>
+
+      </div>
       <div className='previous'>
-        {offset > 0 && <button onClick={goToStart}>Ir al inicio</button>}
-        {offset > 0 && <button onClick={loadPreviousPokemons}>Ver anteriores</button>}
-      </div>      
+        <button onClick={goToStart}>Ir al inicio</button>
+        <button onClick={loadPreviousPokemons}>Ver anteriores</button>
+      </div>
       <ul className='pokemonlist'>
         {filteredPokemons.map((pokemonDetail) => {
           console.log(pokemonDetail);
           return (
             <li key={pokemonDetail.name}>
               <Link to={`/DetailPage/${pokemonDetail.created ? (pokemonDetail.idPokemon || 'unknown') : (pokemonDetail.id || 'unknown')}`}>
-                <img src={pokemonDetail.sprites.front_default} alt={pokemonDetail.name}/>
+                <img src={pokemonDetail.sprites.front_default} alt={pokemonDetail.name} />
                 <h2>
-                    {pokemonDetail.name} 
+                  {pokemonDetail.name}
                 </h2>
               </Link>
             </li>
           );
         })}
       </ul>
-      <div className="next">        
-      {offset +20< allPokemons.length && <button onClick={loadMorePokemons}>Ver más</button>}
-      {offset +20< allPokemons.length && <button onClick={goToEnd}>Ir al final</button>}
+      <div className="next">
+        <button onClick={loadMorePokemons}>Ver más</button>
+        <button onClick={goToEnd}>Ir al final</button>
       </div>
     </div>
   );
